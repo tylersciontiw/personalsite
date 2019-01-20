c98c83c86f7201ed2083da5ffa1f452911cb8c04
Tyler Scionti <tylerscionti@Tylers-MacBook-Pro.local>
2019-01-20 06:43:58 -0500

    deleted all files and starting over because I suck

--- django-react-site/project/blog/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class BlogPost(models.Model):
-    title = models.CharField(max_length=100)
-    tag = models.CharField(max_length = 50)
-    post_body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
