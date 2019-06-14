ec92c41ad080b5b37f83792924122f5dabc1dc96
Tyler Scionti <tylerscionti@Tylers-MacBook-Pro.local>
2019-06-13 21:33:40 -0400

    set static base dir

--- website/website/settings.py
+++ website/website/settings.py
@@ -128,5 +128,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = '/home/TylerScionti/personalsite/blog/static'
+STATIC_ROOT = '/home/tylersciontipersonal/personalsite/blog/static'
 STATIC_URL = '/static/'
